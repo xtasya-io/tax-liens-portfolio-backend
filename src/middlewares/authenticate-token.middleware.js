1e41fabcc7e2c8aadb038c5d6fe19c1927b70561
Ikdemm <ikdem.benmbarek@aiesec.net>
2021-07-27 21:43:16 +0100

    authenticate-token v1 middleware

--- src/middlewares/authenticate-token.middleware.js
+++ src/middlewares/authenticate-token.middleware.js
@@ -1,13 +1,15 @@
 const jwt = require('jsonwebtoken')
+const httpStatus = require("http-status")
 
 module.exports = (req, res, next) => {
     const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
+    const token = (authHeader && authHeader.split(' ')[1]) || req.headers['x-access-token'] || req.query.token;
 
-    if (!token) return res.sendStatus(401);
+    if (!token) return res.sendStatus(httpStatus.FORBIDDEN);
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, data) => {
-        if (err) return res.sendStatus(401);
+        if (err) return res.sendStatus(httpStatus.FORBIDDEN);
+        console.log(data)
         req.data = data;
         next()
     })
