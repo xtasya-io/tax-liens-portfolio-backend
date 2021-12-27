186622f050be6f30f09fc941347249eb198068a9
Ikdemm <ikdem.benmbarek@aiesec.net>
2021-12-27 14:12:35 +0100

    moved token existance condition

--- src/middlewares/authenticate-token.js
+++ src/middlewares/authenticate-token.js
@@ -6,12 +6,14 @@ module.exports = (req, res, next) => {
 
     const authHeader = req.headers['authorization'];
     const token = (authHeader && authHeader.split(' ')[1]) || req.headers['x-access-token'] || req.query.token;
+    
+    if (!token) res.status(httpStatus.FORBIDDEN).send({ message: "No Bearer Token provided"});
+
     if (token && token.startsWith('Bearer ')) {
         // Remove Bearer from string
         token = token.slice(7, token.length);
     }
 
-    if (!token) res.status(httpStatus.FORBIDDEN).send({ message: "No Bearer Token provided"});
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async (err, data) => {
         if (err) {
